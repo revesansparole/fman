68bceccb4ae57b916122eea295fe8217e5fb612e
jchopard <jerome.chopard@itk.fr>
2017-01-11 15:50:44 +0100

    package created with pkglts

---
+++ doc/index.rst
@@ -0,0 +1,21 @@
+
+Welcome to fman's documentation!
+====================================================
+
+Contents:
+
+.. toctree::
+    :maxdepth: 2
+
+    readme
+    installation
+    usagecontributing
+    authors
+    history
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# {# pkglts, pysetup.kwds
+# format setup arguments
+
+from setuptools import setup, find_packages
+
+
+short_descr = "file manager"
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+# find version number in src/fman/version.py
+version = {}
+with open("src/fman/version.py") as fp:
+    exec(fp.read(), version)
+
+
+setup_kwds = dict(
+    name='fman',
+    version=version["__version__"],
+    description=short_descr,
+    long_description=readme + '\n\n' + history,
+    author="revesansparole, ",
+    author_email="revesansparole@gmail.com, ",
+    url='https://github.com/revesansparole/fman',
+    license='mit',
+    zip_safe=False,
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        ],
+    tests_require=[
+        "mock",
+        "nose",
+        ],
+    entry_points={},
+    keywords='',
+    test_suite='nose.collector',
+)
+# #}
+# change setup_kwds below before the next pkglts tag
+
+# do not change things below
+# {# pkglts, pysetup.call
+setup(**setup_kwds)
+# #}
