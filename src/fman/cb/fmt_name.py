e4ad88a58e9594739f186e6c0942fbd95aa69b25
jchopard <jerome.chopard@itk.fr>
2017-01-22 23:54:44 +0100

    use stratageme to bypass windows limitation on majuscule in filenames

--- src/fman/cb/fmt_name.py
+++ src/fman/cb/fmt_name.py
@@ -2,21 +2,32 @@
 """
 
 from glob import glob
-from os import rename
+from os import mkdir, path, rename
 from os.path import exists
 
 from fman.cb.book import Book
+
+tmp_fld = ".tmp_fld"
 
 
 def fmt(filename):
     """Normalize name.
     """
+    if not path.exists(tmp_fld):
+        mkdir(tmp_fld)
+
     name = str(Book(filename))
     if name != filename:
+        # do rename in two steps to avoid troubles with
+        # windows not distinguishing upper from lower
+        # letters in file names
+        rename(filename, path.join(tmp_fld, filename))
         if exists(name):
+            # revert move
+            rename(path.join(tmp_fld, filename), filename)
             raise FileExistsError("CBZ file '{}' already exists".format(name))
         else:
-            rename(filename, name)
+            rename(path.join(tmp_fld, filename), name)
 
 
 def main(filenames=None):
