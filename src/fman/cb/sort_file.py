d167c962a848786895558de9252a42a6aa316098
jchopard <jerome.chopard@itk.fr>
2018-07-14 23:20:11 +0200

    sort comics

--- src/fman/cb/sort_file.py
+++ src/fman/cb/sort_file.py
@@ -1,45 +1,52 @@
 """Script used to sort cbz files in normalized directory architecture
 and associate a hash file with them.
 """
+from pathlib import Path
 
-from glob import glob
-from os import mkdir
-from os.path import basename, exists, join
-from shutil import move
-from sys import argv
-
-from ..integrity import associate_hash
 from . import standard as std
+from ..integrity import associate_hash
 
 
-def sort(filename):
+def sort(pth):
     """Move file in corresponding directory.
+
+    Args:
+      pth (Path): Path to sort
+
+    Returns:
+      (Path|None): tgt path or None if it already exists
     """
-    name = basename(filename)
-    dirname = std.dirname(filename)
-    tgtname = join(dirname, name)
-    if exists(tgtname):
+    pth_tgt = std.dirname(pth.name) / pth.name
+    if pth_tgt.exists():
         return None
     else:
-        move(filename, tgtname)
-        return tgtname
+        pth.rename(pth_tgt)
+        return pth_tgt
 
 
-def sort_comix(filenames=None):
-    """Convert files in current directory or whose names have been
+def sort_comics(pth):
+    """Sort comics in current directory or whose names have been
     passed on the command line.
+
+    Args:
+      pth (Path): Path to sort
+
+    Returns:
+      (None)
     """
-    if filenames is None:
-        filenames = sorted(glob("*.cbz"))
+    if pth.is_dir():
+        books = sorted(pth.glob("*.cbz"))
+    else:
+        books = [pth]
 
-    for dirname in "0abcdefghijklmnopqrstuvwxyz":
-        if not exists(dirname):
-            mkdir(dirname)
+    for dpth in (Path(d) for d in "0abcdefghijklmnopqrstuvwxyz"):
+        if not dpth.exists():
+            dpth.mkdir()
 
-    for filename in filenames:
-        pth = sort(filename)
-        if pth is None:
-            print("already exists: {}".format(filename))
+    for pth in books:
+        pth_sorted = sort(pth)
+        if pth_sorted is None:
+            print(f"already exists: {pth}")
         else:
-            print("moved: {}".format(filename))
-            associate_hash(pth)
+            print(f"moved: {pth}")
+            associate_hash(pth_sorted)
